b7df6d320af8463209f6e4fcac0c9615d02c48db
Aryan Kumar <123313733+kumararyan22@users.noreply.github.com>
2024-09-26 16:45:45 +0530

    Time: 24 ms (74.09%), Space: 46.8 MB (20.63%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+         
+         int n = nums.length;
+         Map<Integer , Integer> map = new HashMap<>();
+         map.put(0,1);  //0 sum and 1 frequency first put in enpty hashmap
+         
+         int sum = 0;
+         int count  = 0;
+
+         for(int i=0; i<n; i++)
+         {
+            sum += nums[i];
+            int diff = sum - k;  //remaining value check in map
+
+            if(map.containsKey(diff))
+            {
+                count += map.get(diff);   //last frequency get and add count variable
+            }
+
+            map.put(sum , map.getOrDefault(sum , 0) + 1);  //otherwise
+         }
+
+         return count;
+        
+    }
+}
