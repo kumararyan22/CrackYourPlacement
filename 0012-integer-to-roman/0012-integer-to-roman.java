59826089b35f1adac6abb663116049a15c435e15
Aryan Kumar <123313733+kumararyan22@users.noreply.github.com>
2024-09-28 20:08:42 +0530

    Time: 3 ms (97.98%), Space: 44.2 MB (76.21%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -4,17 +4,17 @@ class Solution {
         String[] symbols = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
 
-        String res = "";
+        StringBuilder res = new StringBuilder();
 
         for(int i=0; i<symbols.length; i++)  
         {
             while(num >= values[i])  //num check
             {
-                res += symbols[i];  //current symbol add
+                res.append(symbols[i]);  //current symbol add
                 num -= values[i];  //present value out
             }
         }
 
-        return res;
+        return res.toString();
     }
 }
