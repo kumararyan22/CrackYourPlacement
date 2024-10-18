89c2189d946f8e7d66abe6a3add1c61def6d4689
Aryan Kumar <123313733+kumararyan22@users.noreply.github.com>
2024-10-18 23:30:26 +0530

    Time: 0 ms (100%), Space: 43.5 MB (47.85%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -12,45 +12,79 @@
  *         this.right = right;
  *     }
  * }
- */
+//  */
+// class Solution {
+
+//     ArrayList<Integer> result = new ArrayList<>();
+
+//     public void inOrderTraversal(TreeNode node)
+//     {
+//         if(node.left != null)
+//         {
+//             inOrderTraversal(node.left);   //left side
+//         }
+
+//         result.add(node.val);         // mid(root) store
+
+//         if(node.right != null)        //right side
+//         {
+//             inOrderTraversal(node.right);
+//         }
+//     }
+
+
+//     public boolean isValidBST(TreeNode root) {
+
+//         if(root == null)
+//         {
+//             return true;
+//         }
+        
+//         inOrderTraversal(root);    //function calling
+
+//         int n = result.size();
+//         for(int i=0; i<n-1; i++)   
+//         {
+//             if(result.get(i) >= result.get(i+1))    //value check not print
+//             {
+//                 return false;
+//             }
+//         }
+
+//         return true;
+//     }
+// }
+
+
+
+
+//recursion approach
 class Solution {
 
-    ArrayList<Integer> result = new ArrayList<>();
-
-    public void inOrderTraversal(TreeNode node)
-    {
-        if(node.left != null)
-        {
-            inOrderTraversal(node.left);   //left side
-        }
-
-        result.add(node.val);         // mid(root) store
-
-        if(node.right != null)        //right side
-        {
-            inOrderTraversal(node.right);
-        }
+    public boolean isValidBST(TreeNode root) {
+    
+        return Check(root , Long.MIN_VALUE , Long.MAX_VALUE);
     }
 
-
-    public boolean isValidBST(TreeNode root) {
-
+    public boolean Check(TreeNode root , long min , long max)
+    {
         if(root == null)
         {
             return true;
         }
-        
-        inOrderTraversal(root);    //function calling
 
-        int n = result.size();
-        for(int i=0; i<n-1; i++)   
+        if(root.val <= min || root.val >= max)
         {
-            if(result.get(i) >= result.get(i+1))    //value check not print
-            {
-                return false;
-            }
+            return false;
         }
 
-        return true;
+        boolean leftside = Check(root.left , min , root.val);
+        boolean rightside = Check(root.right , root.val , max);
+
+        return (leftside && rightside);
+
+
+        
+        
     }
 }
