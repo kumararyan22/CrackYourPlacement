916fbfc27e33133fa800fd3d1f855480bbbb856e
Aryan Kumar <123313733+kumararyan22@users.noreply.github.com>
2024-10-19 22:14:47 +0530

    Time: 0 ms (100%), Space: 40.3 MB (38.54%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,17 +1,19 @@
 class Solution {
+    
+    int[] dp ;
 
     public int numTrees(int n) {
         
-        int[] dp = new int[n+1];
+        dp = new int[n+1];
         for(int i=0; i<=n; i++)
         {
             dp[i] = -1;
         }
 
-        return solveBST(n , dp);
+        return solveBST(n);
     }
 
-    public int solveBST(int n , int[] dp)
+    public int solveBST(int n)
     {
         if(n == 0 || n == 1)
         {
@@ -26,7 +28,7 @@ class Solution {
         int ans = 0;
         for(int i=1; i<=n; i++)
         {
-            ans += solveBST(i-1 , dp) * solveBST(n-i , dp);
+            ans += solveBST(i-1) * solveBST(n-i);
         }
 
         return dp[n] = ans;
