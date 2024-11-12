7b4035c960b11731b0b59857ab2e27af434a9e38
Aryan Kumar <123313733+kumararyan22@users.noreply.github.com>
2024-11-12 22:48:56 +0530

    Time: 2 ms (38.01%), Space: 44 MB (12.04%) - LeetHub

---
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -0,0 +1,79 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ 
+ class Pair<K, V> {
+    private final K key;
+    private final V value;
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+}
+
+
+class Solution {
+    public int widthOfBinaryTree(TreeNode root) {
+        
+        if(root == null)
+        {
+            return 0;
+        }
+
+        long maxWidth = 0;
+
+        Deque<Pair<TreeNode , Long>> dq = new LinkedList<>();
+        dq.add(new Pair<>(root , 0L));
+
+        while(!dq.isEmpty())
+        {
+            int n = dq.size();
+            long left = dq.peekFirst().getValue();
+            long right = dq.peekLast().getValue();
+             
+            maxWidth = Math.max(maxWidth , right-left+1);
+
+            for(int i=0; i<n; i++)
+            {
+                Pair<TreeNode , Long> currPair = dq.pollFirst();
+                TreeNode currVal = currPair.getKey();
+                long index =  currPair.getValue();
+
+                if(currVal.left != null)
+                {
+                    dq.add(new Pair<>(currVal.left , 2*index+1));
+                }
+                if(currVal.right != null)
+                {
+                    dq.add(new Pair<>(currVal.right , 2*index+2));
+                }
+
+
+            }
+
+        }
+
+        return (int) maxWidth;
+    }
+}
