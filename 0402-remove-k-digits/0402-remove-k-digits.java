307b663d5003a414120b3003b539214a17fd640d
Aryan Kumar <123313733+kumararyan22@users.noreply.github.com>
2024-11-22 16:50:03 +0530

    Time: 23 ms (57.23%), Space: 45.2 MB (74.63%) - LeetHub

---
+++ 0402-remove-k-digits/0402-remove-k-digits.java
@@ -0,0 +1,46 @@
+import java.util.Stack;
+
+class Solution {
+    public String removeKdigits(String num, int k) {
+        int n = num.length();
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            // Remove larger digits from the stack to make the number smaller
+            while (!st.isEmpty() && k > 0 && st.peek() > num.charAt(i)) {
+                st.pop();
+                k--;
+            }
+            // Push the current digit into the stack
+            st.push(num.charAt(i));
+        }
+
+        // If there are still digits to remove, remove them from the end
+        while (k > 0 ) 
+        {
+            st.pop();
+            k--;
+        }
+        if(st.isEmpty())
+        {
+            return "0";
+        }
+
+        StringBuilder res = new StringBuilder();
+        while (!st.isEmpty())
+        {
+            res.append(st.pop());   //all val add last to first(LIFO order)
+        }
+        res.reverse(); // Reverse to get the correct order
+
+        // Remove leading zeros
+        int index = 0;
+        while (index < res.length() && res.charAt(index) == '0') {
+            index++;
+        }
+
+        // If result is empty after removing zeros, return "0"
+        String result = res.substring(index);
+        return result.isEmpty() ? "0" : result;
+    }
+}
