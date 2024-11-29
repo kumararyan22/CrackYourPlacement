0d8cbfc7274ea43449d60ca94345797da8eb4328
Aryan Kumar <123313733+kumararyan22@users.noreply.github.com>
2024-11-29 23:52:01 +0530

    Time: 2 ms (99.6%), Space: 45.1 MB (52.69%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,33 +1,34 @@
 class Solution {
     public String convert(String s, int numRows) {
         
-        if(numRows == 1)
+        if(numRows == 1)   //base case - 1
         {
             return s;
         }
 
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();  //faster
         
-        for(int i=0; i<numRows; i++)
+        for(int i=0; i<numRows; i++)   //row by row check
         {
-            int idx = i;
-            int southCount = 2 * (numRows - 1 - i);
+            int idx = i;  //first character first row always include
+            int southCount = 2 * (numRows - 1 - i);   //dry run best understood
             int northCount = 2 * i;
+
             boolean toggle = true;
 
             while(idx < s.length())
             {
-                sb.append(s.charAt(idx));
+                sb.append(s.charAt(idx));   //base case - 2
 
-                if(i == 0)
+                if(i == 0)  //for the first row 
                 {
                     idx += southCount;
                 }
-                else if(i == numRows-1)
+                else if(i == numRows-1)  //for the last row
                 {
                     idx += northCount;
                 }
-                else
+                else  //otherwise middle row
                 {
                     if(toggle == true)
                     {
