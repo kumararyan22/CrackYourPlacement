ec7adf5623b9b6c3c90db0b2926765a0d2d01df3
Aryan Kumar <123313733+kumararyan22@users.noreply.github.com>
2024-12-09 19:54:12 +0530

    Time: 2 ms (44.94%), Space: 41.9 MB (84.89%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -6,7 +6,7 @@ class Solution {
         int carry = 0;
 
         StringBuilder res = new StringBuilder();
-
+    
         while(n1 >= 0 || n2 >= 0 || carry == 1)
         {
             int bitA = (n1 >= 0) ? a.charAt(n1) - '0': 0;
@@ -14,17 +14,18 @@ class Solution {
 
             int sum = bitA + bitB + carry;
             
-            carry = sum / 2;
+            carry = sum / 2;   //by 2 use bcz binary data only 0 ,1 contains 
+
             res.append(sum % 2);
 
             
 
-            n1--;
+            n1--;  //both are one decrement 
             n2--;
 
         }
         
-        return res.reverse().toString();
+        return res.reverse().toString();  
         
     }
 }
