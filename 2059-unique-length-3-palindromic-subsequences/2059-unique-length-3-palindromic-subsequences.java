6694ccc64743c2cacc33befd939663d4eab76402
Aryan Kumar <123313733+kumararyan22@users.noreply.github.com>
2025-01-04 23:13:27 +0530

    Time: 241 ms (28.79%), Space: 45.4 MB (60.21%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -2,7 +2,7 @@ class Solution {
     public int countPalindromicSubsequence(String s) {
         
         int n = s.length();
-        Set<Character> letters = new HashSet<>();
+        Set<Character> letters = new HashSet<>(26);
         for(int i=0; i<n; i++)
         {
             letters.add(s.charAt(i));
@@ -26,7 +26,7 @@ class Solution {
                 }
             }
 
-            Set<Character> set = new HashSet<>();
+            Set<Character> set = new HashSet<>(26);
             for(int middle = left_idx+1; middle <= right_idx-1; middle++)
             {
                 set.add(s.charAt(middle));
