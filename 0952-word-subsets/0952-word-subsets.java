b61a5a32eb2ca0ec35147200132743e7788530d4
Aryan Kumar <123313733+kumararyan22@users.noreply.github.com>
2025-01-10 23:14:14 +0530

    Time: 13 ms (81.4%), Space: 54.9 MB (48.84%) - LeetHub

---
+++ 0952-word-subsets/0952-word-subsets.java
@@ -0,0 +1,46 @@
+class Solution {
+
+    public boolean isSubset(int[] freq2 , int[] temp)
+    {
+        for(int i=0; i<26; i++)
+        {
+            if(temp[i] < freq2[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public List<String> wordSubsets(String[] words1, String[] words2) {
+        List<String> result = new ArrayList<>();
+
+        int[] freq2 = new int[26];
+        for(String word : words2)
+        {
+            int[] temp = new int[26];
+            for(char ch : word.toCharArray())
+            {
+                temp[ch-'a']++;
+
+                freq2[ch-'a'] = Math.max(freq2[ch-'a'] , temp[ch-'a']);
+            }
+        }
+
+        for(String word : words1)
+        {
+            int[] temp = new int[26];
+            for(char ch : word.toCharArray())
+            {
+                temp[ch-'a']++;
+            }
+
+            if(isSubset(freq2 , temp) == true)
+            {
+                result.add(word);
+            }
+        }
+
+        return result;
+    }
+}
