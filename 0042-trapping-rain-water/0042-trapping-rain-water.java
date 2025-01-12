9ea2f115fa34a52c11ed05e5a06dc0cbb4f0657c
Aryan Kumar <123313733+kumararyan22@users.noreply.github.com>
2025-01-12 22:15:52 +0530

    Time: 1 ms (64.46%), Space: 46.3 MB (62.07%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -2,24 +2,24 @@ class Solution {
     public int trap(int[] height) {
       
       int n = height.length;
-      if(n == 0 || n < 3)   //base case
+      if(n == 0 || n < 3)   //base case , if less than 3 height so water not store any place
       {
         return 0;
       }
 
-      int[] maxL = new int[n];
+      int[] maxL = new int[n];   //for lest side - find greatest height
       maxL[0] = height[0];
       for(int i=1; i<n; i++)
       {
-        maxL[i] = Math.max(maxL[i-1] , height[i]);
+        maxL[i] = Math.max(maxL[i-1] , height[i]);  
       }
       
 
-      int[] maxR = new int[n];
-      maxR[n-1] = height[n-1];
+      int[] maxR = new int[n];    //for right side - find greatest height
+      maxR[n-1] = height[n-1];    //first height store
       for(int i=n-2; i>=0; i--)
       {
-        maxR[i] = Math.max(maxR[i+1] , height[i]);
+        maxR[i] = Math.max(maxR[i+1] , height[i]);  //bcz i+1 , last height reach
       }
 
       int[] water = new int[n];
