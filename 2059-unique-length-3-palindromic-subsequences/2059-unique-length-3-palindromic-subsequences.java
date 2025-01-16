f4dd8845b428ec08faa1c14ba34543b9cd3be1d8
Aryan Kumar <123313733+kumararyan22@users.noreply.github.com>
2025-01-16 22:54:07 +0530

    Time: 252 ms (20.01%), Space: 45.4 MB (82.44%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -5,34 +5,34 @@ class Solution {
         Set<Character> letters = new HashSet<>(26);
         for(int i=0; i<n; i++)
         {
-            letters.add(s.charAt(i));
+            letters.add(s.charAt(i));  //unique character store
         }
 
         int result = 0;
-        for(char letter : letters)
+        for(char letter : letters)   //one by one unique char iterate
         {
-            int left_idx = -1;
+            int left_idx = -1;  //initialize both -1 firstly
             int right_idx = -1;
 
-            for(int i=0; i<n; i++)
+            for(int i=0; i<n; i++)  //iterate again string
             {
-                if(s.charAt(i) == letter)
+                if(s.charAt(i) == letter)  //if equal
                 {
                     if(left_idx == -1)
                     {
-                        left_idx = i;
+                        left_idx = i;  //first time update only
                     }
-                    right_idx = i;
+                    right_idx = i;  //it is again and again update ultil the last index not found same
                 }
             }
 
             Set<Character> set = new HashSet<>(26);
-            for(int middle = left_idx+1; middle <= right_idx-1; middle++)
+            for(int middle = left_idx+1; middle <= right_idx-1; middle++)  //middle char again new set store (dry run best)
             {
                 set.add(s.charAt(middle));
             }
 
-            result += set.size();
+            result += set.size();  //add size of middle char 
         }
        
        return result;
