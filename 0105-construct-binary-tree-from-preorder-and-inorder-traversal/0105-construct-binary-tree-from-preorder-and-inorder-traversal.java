186b6343f47716e4acb0d3dfe51960332eae9518
Aryan Kumar <123313733+kumararyan22@users.noreply.github.com>
2025-02-21 23:00:50 +0530

    Time: 3 ms (38.17%), Space: 44.3 MB (62.03%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,22 +1,23 @@
 class Solution {
-    int idx = 0;
+    int idx = 0;   //globaly define
 
     public TreeNode solve(int[] preorder, int[] inorder, int start, int end) {
-        if (start > end) {
+        if (start > end) {   //base case
             return null;
         }
 
-        int rootVal = preorder[idx++];
-        int i = start;
+        int rootVal = preorder[idx];
+        TreeNode root = new TreeNode(rootVal); 
+        idx++;  //every iteration increment
 
+        int i = start;
         for (; i <= end; i++) {
-            if (inorder[i] == rootVal) {
-                break;
+            if (inorder[i] == rootVal) {   //if find 
+                break;   //then stop this location
             }
         }
 
-        TreeNode root = new TreeNode(rootVal); 
-        root.left = solve(preorder, inorder, start, i - 1);
+        root.left = solve(preorder, inorder, start, i - 1);   //recursion call
         root.right = solve(preorder, inorder, i + 1, end);
 
         return root;
@@ -25,7 +26,6 @@ class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) 
     {
         int n = inorder.length;
-        // idx = 0;
         return solve(preorder, inorder, 0, n - 1);
     }
 }
