c71aa562af0f8b012edf933a4364ceb4c218455d
Aryan Kumar <123313733+kumararyan22@users.noreply.github.com>
2025-02-23 22:49:55 +0530

    Time: 0 ms (100%), Space: 43.3 MB (67.41%) - LeetHub

--- 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.java
+++ 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.java
@@ -22,30 +22,30 @@ class Solution {
 
     public static TreeNode dfs(int[] preorder, int pst, int pei , int[] postorder , int ppst, int ppei)
     {
-         if(pst > pei)
+         if(pst > pei)   //base case
          {
             return null;
          }
 
-        TreeNode root = new TreeNode(preorder[pst]);
-         if(pst == pei)
+        TreeNode root = new TreeNode(preorder[pst]);   //root value store
+         if(pst == pei)   //if only one value exists
          {
             return root;
          }
 
 
          int idx = ppst;
-         while(preorder[pst+1] != postorder[idx])
+         while(preorder[pst+1] != postorder[idx])   //untill value not find in postorder
          {
             idx++;
          }
 
-         int len = idx - ppst + 1;
+         int len = idx - ppst + 1;  //total length(left side)
 
-         root.left = dfs(preorder,pst+1,pst+len  ,  postorder,ppst,idx);
+         root.left = dfs(preorder,pst+1,pst+len  ,  postorder,ppst,idx);     //recursion , dry run
          root.right = dfs(preorder,pst+len+1,pei  ,  postorder,idx+1,ppei-1);
 
-         return root;
+         return root;  //answer
 
     }
 }
