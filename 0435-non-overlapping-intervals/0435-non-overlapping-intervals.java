8647e0b38708e6cffe1cd74f3777e742fcf492df
Aryan Kumar <123313733+kumararyan22@users.noreply.github.com>
2025-02-25 01:28:26 +0530

    Time: 45 ms (95.61%), Space: 73.8 MB (36.4%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -7,10 +7,10 @@ class Solution {
         
         Arrays.sort(intervals, (a,b) -> a[1] - b[1]);
 
-        int count = 1;
-        int lastEndTime = intervals[0][1];
+        int count = 1;   //minimum one always non-overlapping
+        int lastEndTime = intervals[0][1];  
 
-        for(int i=0; i<n; i++)
+        for(int i=1; i<n; i++)
         {
             if(intervals[i][0] >= lastEndTime)
             {
