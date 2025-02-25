81a352dabaf5b8ef6a1ea089fe4a0f026b72282a
Aryan Kumar <123313733+kumararyan22@users.noreply.github.com>
2025-02-25 22:31:00 +0530

    Time: 118 ms (11.78%), Space: 57.4 MB (69.71%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,88 +1,32 @@
 
-
-class Pair {
-    int heights;
-    int index;
-
-    public Pair(int heights, int index) {
-        this.heights = heights;
-        this.index = index;
-    }
-}
-
 class Solution {
 
-    public static int[] Left(int[] heights) { // for nearest smaller to left
+    private int[] Left(int[] heights) { // Nearest smaller to left
         int n = heights.length;
-        int psedo = -1;
         int[] res1 = new int[n];
-        Stack<Pair> st = new Stack<>();
+        Stack<Integer> st = new Stack<>();
 
         for (int i = 0; i < n; i++) {
-           if(st.size() == 0)
-           {
-            res1[i] = psedo;
-           }
-           else if(st.size() > 0 && st.peek().heights < heights[i])
-           {
-             res1[i] = st.peek().index;
-           }
-           else
-           {
-            while(st.size() > 0 && st.peek().heights >= heights[i])
-            {
+            while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
                 st.pop();
             }
-            if(st.size() == 0)
-            {
-                res1[i] = psedo;
-            }
-            else
-            {
-                res1[i] = st.peek().index;
-            }
-           }
-
-           st.push(new Pair(heights[i] , i));
+            res1[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
         }
         return res1;
     }
 
-    public static int[] Right(int[] heights) { // for nearest smaller to right
+    private int[] Right(int[] heights) { // Nearest smaller to right
         int n = heights.length;
-        int psedo = n;
         int[] res2 = new int[n];
-        Stack<Pair> st = new Stack<>();
-        
-        for (int i = n-1; i>=0; i--)
-         {
-           if(st.size() == 0)
-           {
-            res2[i] = psedo;
-           }
-           else if(st.size() > 0 && st.peek().heights < heights[i])
-           {
-             res2[i] = st.peek().index;
-           }
-           else
-           {
-            while(st.size() > 0 && st.peek().heights >= heights[i])
-            {
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
                 st.pop();
             }
-            if(st.size() == 0)
-            {
-                res2[i] = psedo;
-            }
-            else
-            {
-                res2[i] = st.peek().index;
-            }
-           }
-
-           st.push(new Pair(heights[i] , i));
-        
-        
+            res2[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
         }
         return res2;
     }
@@ -91,7 +35,7 @@ class Solution {
         int n = heights.length;
         int[] left = Left(heights);
         int[] right = Right(heights);
-        int max = Integer.MIN_VALUE;
+        int max = 0;
 
         for (int i = 0; i < n; i++) {
             int width = right[i] - left[i] - 1;
