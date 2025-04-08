b64be4070c3db5731f4c035c1e4796b24ae94efd
Aryan Kumar <123313733+kumararyan22@users.noreply.github.com>
2025-04-08 23:40:03 +0530

    Time: 2 ms (72.10%) | Memory: 44.2 MB (73.87%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.java
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -1,38 +1,54 @@
 class Solution {
     public int minimumOperations(int[] nums) {
         
+    //     int n = nums.length;     //brute force(codestorywithMIK)
+    //     int operation = 0;
+
+    //     for(int i=0; i<n; i+=3)
+    //     {
+    //         if(check(nums,n,i) == true)
+    //         {
+    //             return operation;
+    //         }
+    //         else
+    //         {
+    //             operation++;
+    //         }
+    //     }
+
+    //     return operation;
+    // }
+
+    // public boolean check(int[] nums, int n, int idx)
+    // {
+    //     Set<Integer> set = new HashSet<>();
+    //     for(int j=idx; j<n; j++)
+    //     {
+    //         if(set.contains(nums[j]))
+    //         {
+    //             return false;
+    //         }
+    //         set.add(nums[j]);
+    //     }
+
+    //     return true;
+
+
+
+
         int n = nums.length;
-        int operation = 0;
-
-        for(int i=0; i<n; i+=3)
-        {
-            if(check(nums,n,i) == true)
-            {
-                return operation;
-            }
-            else
-            {
-                operation++;
-            }
-        }
-
-        return operation;
-    }
-
-    public boolean check(int[] nums, int n, int idx)
-    {
         Set<Integer> set = new HashSet<>();
-        for(int j=idx; j<n; j++)
+
+        for(int i=n-1; i>=0; i--)
         {
-            if(set.contains(nums[j]))
+            if(set.contains(nums[i]))
             {
-                return false;
+                return (int)Math.ceil((i+1)/3.0);
             }
-            set.add(nums[j]);
+            set.add(nums[i]);
         }
 
-        return true;
-
+        return 0;
     }
 
 
