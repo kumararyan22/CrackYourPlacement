13c3697432d701319e030f6d8f25c914f6e07f12
Aryan Kumar <123313733+kumararyan22@users.noreply.github.com>
2025-04-24 22:55:11 +0530

    Time: 7 ms (89.07%) | Memory: 45.5 MB (6.89%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
@@ -1,30 +1,66 @@
 class Solution {
     public int countCompleteSubarrays(int[] nums) {
         
+        // int n = nums.length;
+        // Set<Integer> set1 = new HashSet<>();
+        // for(int i=0; i<n; i++)
+        // {
+        //     set1.add(nums[i]);
+        // }
+
+        // int size = set1.size();
+        // int count = 0;
+
+        // for(int i=0; i<n; i++)
+        // {
+        //     Set<Integer> set2 = new HashSet<>();
+        //     for(int j=i; j<n; j++)
+        //     {
+        //         set2.add(nums[j]);
+
+        //         if(size == set2.size())
+        //         {
+        //             count++;
+        //         }
+        //     }
+        // }
+
+        // return count;
+
+
+
         int n = nums.length;
-        Set<Integer> set1 = new HashSet<>();
-        for(int i=0; i<n; i++)
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums)
         {
-            set1.add(nums[i]);
+            set.add(num);
         }
 
-        int size = set1.size();
+        int i=0;
+        int j=0;
         int count = 0;
+        Map<Integer,Integer> map = new HashMap<>();
 
-        for(int i=0; i<n; i++)
+        while(j < n)
         {
-            Set<Integer> set2 = new HashSet<>();
-            for(int j=i; j<n; j++)
+            map.put(nums[j] , map.getOrDefault(nums[j],0)+1);
+            
+            while(set.size() == map.size())
             {
-                set2.add(nums[j]);
-
-                if(size == set2.size())
+                count += n-j;
+                map.put(nums[i], map.get(nums[i])-1);
+                if(map.get(nums[i]) == 0)
                 {
-                    count++;
+                    map.remove(nums[i]);
                 }
+
+                i++;
             }
+
+            j++;
         }
 
         return count;
+
     }
 }
