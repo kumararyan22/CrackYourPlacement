e2de8aeb888a599b0e93101311dbda77157ba70a
Aryan Kumar <123313733+kumararyan22@users.noreply.github.com>
2025-06-05 14:56:38 +0530

    Time: 10 ms (98.37%) | Memory: 57.5 MB (17.17%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,55 +1,60 @@
 class Solution {
+    Boolean[][] dp;
+
     public boolean canPartition(int[] nums) {
         
         int n = nums.length;
-        int sum = 0;
+        int totalSum = 0;
+
         for(int i=0; i<n; i++)
         {
-            sum += nums[i];
+            totalSum += nums[i];
         }
 
-        if(sum % 2 != 0)
+        if(totalSum % 2 != 0)
+        {
+            return false;
+        }
+        else
+        {
+            return helper(nums,totalSum / 2);
+        }
+    }
+
+    public boolean helper(int[] nums,int sum)
+    {
+        int n = nums.length;
+        dp = new Boolean[n+1][sum+1];
+
+        return solveSubset(nums,n,sum);
+
+    }
+
+    public boolean solveSubset(int[] nums, int n, int sum)
+    {
+        if(sum == 0)
+        {
+            return true;
+        }
+        if(n == 0)
         {
             return false;
         }
 
-        return subsetSum(nums , n, sum/2);
-    }
-
-    public boolean subsetSum(int[] nums, int n, int sum)
-    {
-        boolean[][] tb = new boolean[n+1][sum+1];
-
-        for(int i=0; i<=n; i++)
+        if(dp[n][sum] != null)
         {
-            for(int j=0; j<=sum; j++)
-            {
-                if(j == 0)
-                {
-                    tb[i][j] = true;
-                }
-                else if(i == 0)
-                {
-                    tb[i][j] = false;
-                }
-            }
+            return dp[n][sum];
         }
 
-        for(int i=1; i<=n; i++)
+        if(nums[n-1] <= sum)
         {
-            for(int j=1; j<=sum; j++)
-            {
-                if(nums[i-1] <= j)
-                {
-                    tb[i][j] = tb[i-1][j-nums[i-1]] || tb[i-1][j];
-                }
-                else
-                {
-                    tb[i][j] = tb[i-1][j];
-                }
-            }
+            dp[n][sum] = solveSubset(nums,n-1,sum-nums[n-1]) || solveSubset(nums,n-1,sum);
+        }
+        else
+        {
+            dp[n][sum] = solveSubset(nums,n-1,sum);
         }
 
-        return tb[n][sum];
+        return dp[n][sum];
     }
 }
