29f2cfbc9645e0d31e8074c6ddaa64cc1183e7b0
Aryan Kumar <123313733+kumararyan22@users.noreply.github.com>
2025-06-18 16:52:36 +0530

    Time: 23 ms (81.15%) | Memory: 59.2 MB (80.63%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.java
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.java
@@ -1,29 +1,59 @@
 class Solution {
     public int[][] divideArray(int[] nums, int k) {
         
+        // int n = nums.length;
+        // int[][] ans = new int[n/3][3];
+        // int row=0;
+
+        // Arrays.sort(nums);
+
+        // for(int i=0; i<=n-3; i+=3)
+        // {
+        //     int[] arr = new int[3];
+        //     int idx = 0;
+        
+        //     for(int j=i; j<i+3; j++)
+        //     {
+        //         if(nums[j]-nums[i] <= k)
+        //         {
+        //             arr[idx++] = nums[j];
+        //         }
+        //         else
+        //         {
+        //             return new int[0][0];
+        //         }
+        //     }
+        //     ans[row] = arr;
+        //     row++;
+        // }
+
+        // return ans;
+
+
+
         int n = nums.length;
+        Arrays.sort(nums);
         int[][] ans = new int[n/3][3];
         int row=0;
 
-        Arrays.sort(nums);
-
         for(int i=0; i<=n-3; i+=3)
         {
-            int[] arr = new int[3];
-            int idx = 0;
+            int a = nums[i];
+            int b = nums[i+1];
+            int c = nums[i+2];
 
-            for(int j=i; j<i+3; j++)
+            int min = Math.min(a,Math.min(b,c));
+            int max = Math.max(a,Math.max(b,c));
+
+            if(max - min > k)
             {
-                if(nums[j]-nums[i] <= k)
-                {
-                    arr[idx++] = nums[j];
-                }
-                else
-                {
-                    return new int[0][0];
-                }
+                return new int[0][0];
             }
-            ans[row] = arr;
+
+            ans[row][0] = a;
+            ans[row][1] = b;
+            ans[row][2] = c;
+
             row++;
         }
 
