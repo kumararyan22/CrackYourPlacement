b2abea66d21b6453151e2815a8a10a8edc8f0840
Aryan Kumar <123313733+kumararyan22@users.noreply.github.com>
2025-07-01 02:11:05 +0530

    Time: 2 ms (98.85%) | Memory: 55.1 MB (52.17%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
@@ -4,20 +4,20 @@ class Solution {
         int n = cardPoints.length;
         int leftSum=0;
         
-        for(int i=0; i<k; i++)
+        for(int i=0; i<k; i++)   //all k prefix sum find first,
         {
             leftSum += cardPoints[i];
         }
     
-        int maxSum = leftSum;
-        int rightSum = 0;
-        int rightIdx = n-1;
+        int maxSum = leftSum;  //maxSum , initialize leftSum, bcz it is one possible my answer
+        int rightSum = 0;  
+        // int rightIdx = n-1;  
         
         for(int i=k-1; i>=0; i--)
         {
             leftSum = leftSum - cardPoints[i];
-            rightSum = rightSum + cardPoints[rightIdx];
-            rightIdx--;
+            rightSum = rightSum + cardPoints[n-1];
+            n--;
 
             maxSum = Math.max(maxSum , leftSum + rightSum);
         }
